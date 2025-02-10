fbcb9ca7b0e55925a6ad2cd58f9a55ceb0eaa199
Christopher Flores Fiestas <cflores@PANOSA.COM>
2025-02-10 16:28:39 -0500

    Correo

--- config.js
+++ config.js
@@ -6,5 +6,5 @@ export const DB_PASSWORD = process.env.DB_PASSWORD || ''
 export const DN_NAME=process.env.DN_NAME || 'constancias'
 export const DN_PORT=process.env.DN_PORT || '3306'
 export const EMAIL_USER = 'Floresfiestaschristopher@gmailc.com';
-export const EMAIL_PASS = 'pfuo bduo lwmc whwc';
+export const EMAIL_PASS = 'wbez oqod frhc mbtf';
 export const SERVER_URL = process.env.SERVER_URL|| 'constancias.up.railway.app';
